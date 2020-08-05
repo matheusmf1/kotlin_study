f3a049da656c5a0d0add1163b4edfc9358f80cb7
Matheus Mandotti <matheus.franco@setis.com.br>
2020-08-05 12:24:48 -0300

    Add project architect

---
+++ NyBooksMVVM/app/src/main/java/br/com/matheus/nybooksmvvm/presentation/books/BooksActivity.kt
@@ -0,0 +1,12 @@
+package br.com.matheus.nybooksmvvm.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.matheus.nybooksmvvm.R
+
+class BooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
---
+++ financasKotlin/app/src/main/java/br/com/matheus/financaskotlin/delegate/TransacaoDelegate.kt
@@ -0,0 +1,4 @@
+package br.com.matheus.financaskotlin.delegate
+
+interface TransacaoDelegate {
+}
