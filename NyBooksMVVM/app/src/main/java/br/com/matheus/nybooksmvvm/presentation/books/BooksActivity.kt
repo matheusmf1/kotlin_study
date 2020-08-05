a4b9a948cc09628bcd61dc30af5c3cda547c4738
Matheus Mandotti <matheus.franco@setis.com.br>
2020-08-05 12:54:03 -0300

    Implement toolbar and recyclerView

--- NyBooksMVVM/app/src/main/java/br/com/matheus/nybooksmvvm/presentation/books/BooksActivity.kt
+++ NyBooksMVVM/app/src/main/java/br/com/matheus/nybooksmvvm/presentation/books/BooksActivity.kt
@@ -3,10 +3,14 @@ package br.com.matheus.nybooksmvvm.presentation.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.matheus.nybooksmvvm.R
+import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        toolbarMain.title = "Ny Books"
+        setSupportActionBar( toolbarMain )
     }
 }
